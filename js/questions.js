5a67a100c8e1236fe81754a5a612959450b71977
ROHITCHANDRAN R <68961287+Rohitchandran242@users.noreply.github.com>
2021-12-08 19:50:39 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,4 +1,4 @@
-// creating an array and passing the number, questions, options, and answers
+
 let questions = [
     {
     numb: 1,
@@ -55,18 +55,5 @@ let questions = [
       "1%"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
