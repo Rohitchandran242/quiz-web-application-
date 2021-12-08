0f9c4ec3fd5e0213b1822c982e819b571193e6fb
ROHITCHANDRAN R <68961287+Rohitchandran242@users.noreply.github.com>
2021-12-08 19:48:04 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of the following generally accounts for the major use of household water?",
+    answer: "Landscaping",
+    options: [
+      "Landscaping",
+      "Shower",
+      "Dishwasher",
+      "Washingmachine"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How much of the water on earth is available for peoples everyday use?",
+    answer: "Less than 1%",
+    options: [
+      "More than 25%",
+      "Less than 1%",
+      "10%",
+      "25%"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In the average household, which of the following wastes the most water?",
+    answer: "Leaky toilet",
+    options: [
+      "Leaky toilet",
+      "Running water while washing dishes",
+      "Taking a long shower",
+      "Running water while brushing teeth"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A water-efficient irrigation system may include which features?",
+    answer: "All of the above",
+    options: [
+      "Drip irrigation",
+      "Rain shut off devices",
+      "Moisture sensors",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Easily corrected household water leaks account for what % of the water bill?",
+    answer: "8%",
+    options: [
+      "8%",
+      "10%",
+      "18%",
+      "1%"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
